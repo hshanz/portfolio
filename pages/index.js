b665fe5f4ce4cb6470ff1434a193b22b930d238d
hshanz <david00gbg@hotmail.com>
2022-06-01 16:27:59 +0200

    Init and typewriter

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="startpage-holder">
+            <div className="typew-holder">
+                <h1 className="typew-text"> Hello, my name is David.</h1>
+                <p className="typew-subtitle">Welcome to my portfolio</p>
+            </div>
+        </div>
+  )
+}
+
+export default Home;
